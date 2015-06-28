009ba224bcbd823d08a7b415617aa8538c61aaf4
taranamol <tarskaur@gmail.com>
2015-06-28 16:34:27 -0700

    adding first draft

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,5 +1,31 @@
 // wait for the DOM to finish loading
 window.addEventListener("DOMContentLoaded", function() {
   // all code goes here
-
+alert("ready to play tic tac toe?");
 });
+
+document.querySelector("#col1row1");
+
+col1row1.onclick=function(event) {
+  col1row1.style.backgroundColor = "green";
+};
+
+// document.querySelector("#col2row1");
+
+// col2row1.onclick=function(event) {
+//   col2row1.style.backgroundColor = "green";
+// };
+
+// document.querySelector("#col3row1");
+
+// col3row1.onclick=function(event) {
+//   col3row1.style.backgroundColor = "green";
+// };
+
+
+// var squareId = document.querySelector("#col1row1")
+
+// squareId.addEventListener("click", function() {
+//      event.preventDefault(); 
+//      squareId.style.background-color = "green";
+// })
