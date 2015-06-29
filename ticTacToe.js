832fb6e74b43157652367bfc3212560ea3c040e4
taranamol <tarskaur@gmail.com>
2015-06-28 17:52:03 -0700

    the working code to turn boxes green

--- ticTacToe.js
+++ ticTacToe.js
@@ -2,30 +2,59 @@
 window.addEventListener("DOMContentLoaded", function() {
   // all code goes here
 alert("ready to play tic tac toe?");
+
+var squareId1 = document.querySelector("#col1row1")
+squareId1.addEventListener("click", function() {
+     event.preventDefault(); 
+     squareId1.style.backgroundColor = 'green'
+})
+
+var squareId2 = document.querySelector("#col2row1")
+squareId2.addEventListener("click", function() {
+     event.preventDefault(); 
+     squareId2.style.backgroundColor = 'green'
+})
+
+var squareId3 = document.querySelector("#col3row1")
+squareId3.addEventListener("click", function() {
+     event.preventDefault(); 
+     squareId3.style.backgroundColor = 'green'
+})
+
+var squareId4 = document.querySelector("#col1row2")
+squareId4.addEventListener("click", function() {
+     event.preventDefault(); 
+     squareId4.style.backgroundColor = 'green'
+})
+
+var squareId5 = document.querySelector("#col2row2")
+squareId5.addEventListener("click", function() {
+     event.preventDefault(); 
+     squareId5.style.backgroundColor = 'green'
+})
+
+var squareId6 = document.querySelector("#col3row2")
+squareId6.addEventListener("click", function() {
+     event.preventDefault(); 
+     squareId6.style.backgroundColor = 'green'
+})
+
+var squareId7 = document.querySelector("#col1row3")
+squareId7.addEventListener("click", function() {
+     event.preventDefault(); 
+     squareId7.style.backgroundColor = 'green'
+})
+
+var squareId8 = document.querySelector("#col2row3")
+squareId8.addEventListener("click", function() {
+     event.preventDefault(); 
+     squareId8.style.backgroundColor = 'green'
+})
+
+var squareId9 = document.querySelector("#col3row3")
+squareId9.addEventListener("click", function() {
+     event.preventDefault(); 
+     squareId9.style.backgroundColor = 'green'
+})
+
 });
-
-document.querySelector("#col1row1");
-
-col1row1.onclick=function(event) {
-  col1row1.style.backgroundColor = "green";
-};
-
-// document.querySelector("#col2row1");
-
-// col2row1.onclick=function(event) {
-//   col2row1.style.backgroundColor = "green";
-// };
-
-// document.querySelector("#col3row1");
-
-// col3row1.onclick=function(event) {
-//   col3row1.style.backgroundColor = "green";
-// };
-
-
-// var squareId = document.querySelector("#col1row1")
-
-// squareId.addEventListener("click", function() {
-//      event.preventDefault(); 
-//      squareId.style.background-color = "green";
-// })
